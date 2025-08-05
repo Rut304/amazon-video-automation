b8bf794dbd38dcee67885e63e99d7eaeadcabdc5
Rut304 <jeff.rutman@gmail.com>
2025-08-05 00:50:01 -0400

    Update fetch_products.py

--- scripts/fetch_products.py
+++ scripts/fetch_products.py
@@ -1,20 +1,19 @@
 def fetch_products():
-    # Your implementation here
     return [
         {
-            "title": "Logitech MX Master 3",
+            "name": "Logitech MX Master 3",
             "price": 89.99,
             "rating": 4.7,
             "url": "https://www.amazon.com/dp/B07YFP8KV3",
         },
         {
-            "title": "Razer DeathAdder V2",
+            "name": "Razer DeathAdder V2",
             "price": 59.00,
             "rating": 4.6,
             "url": "https://www.amazon.com/dp/B082G5SPR5",
         },
         {
-            "title": "Corsair Harpoon RGB Wireless",
+            "name": "Corsair Harpoon RGB Wireless",
             "price": 49.99,
             "rating": 4.4,
             "url": "https://www.amazon.com/dp/B07X9VG6Z4",
